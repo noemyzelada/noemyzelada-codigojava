3db147205667152f484767a966cfb975a577c104
Administrador <Administrador@172.16.13.105>
2017-09-08 14:40:14 -0600

    Mi primer subida al repositorio

---
+++ src/practica2_java/Practica2_java.java
@@ -0,0 +1,13 @@
+
+package practica2_java;
+
+class Practica2_java {
+
+   
+    public static void main(String[] args) {
+        
+        System.out.println("hola mundo");
+        
+    }
+    
+}
